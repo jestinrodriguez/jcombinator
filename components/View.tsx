0258470e83812050dd0668c8d396a24695473842
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-11-26 15:32:07 -0800

    sanity write client

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,13 @@
 import { STARTUP_VIEWS_QUERY } from "@/lib/queries"
 import Ping from "./Ping"
 import { client } from "@/sanity/lib/client"
+import { writeClient } from "@/sanity/lib/write-client"
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
-
-    //TODO: Update the number of views
+  
+  after(async () => await writeClient.patch(id).set({ views: totalViews + 1 }).commit());
 
   return (
     <div className="view-container">
