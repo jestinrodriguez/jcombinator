139a6b834072ccd5fb1ff95fa417cc146ee8a570
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-11-25 18:13:31 -0800

    Sanity Live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,15 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/lib/queries";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string}>
 }) {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUPS_QUERY);
   
+  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY})
+
   return (
     <>
       <section className="pink_container">
@@ -36,6 +36,8 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+
+      <SanityLive/>
     </>
   );
 }
