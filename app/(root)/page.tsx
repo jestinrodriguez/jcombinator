80d0f3aff3a7d0ba7b70ccdf9eb8314a300e8588
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-12-04 07:23:45 -0800

    author authentication flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,15 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string}>
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session = await auth();
 
   const { data: posts } = await sanityFetch({query: STARTUPS_QUERY, params})
 
