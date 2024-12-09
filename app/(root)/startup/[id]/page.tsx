d90956eb92e84950cafca5d7175698bd572ae2c7
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-12-09 08:29:37 -0800

    implemented parallel fetching

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { STARTUP_BY_ID_QUERY } from '@/lib/queries';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/lib/queries';
 import { client } from '@/sanity/lib/client';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,6 +9,7 @@ import markdownit from 'markdown-it';
 import { formatDate } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 
 const md = markdownit();
 
@@ -17,7 +18,12 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }>}) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
 
   if(!post) return notFound();
 
@@ -68,7 +74,17 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
           )}
         </div>
         <hr className="divider"/>
-        {/* TODO: EDITOR SELECTED STARTUPS */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
           <Suspense fallback={<Skeleton className="view_skeleton" />}>
             <View id={id}/>
           </Suspense>
